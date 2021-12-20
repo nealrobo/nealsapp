c348518e5fa8bbb8a313922c8ee64cceb8a965aa
nealrobo1901 <neal.a.robinson@googlemail.com>
2021-12-20 22:55:41 +0000

    version one

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nealsapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
