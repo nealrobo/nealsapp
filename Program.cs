aee661ccab1f38f38f35ce6239a9e7cbc27b6799
nealrobo1901 <neal.a.robinson@googlemail.com>
2021-12-21 12:52:57 +0000

    final extension. to merge

--- Program.cs
+++ Program.cs
@@ -7,13 +7,6 @@ namespace nealsapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
